2bc3f2f2c5e5cde944ca5ac33cb35d31909fb325
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-04-17 01:35:28 -0300

    ONS ok

--- file/ons.py
+++ file/ons.py
@@ -1,43 +1,46 @@
-'''
-Created on 21 de ago de 2017
+"""
+Created on 21 de mar de 2018
 
 @author: clebson
-'''
-from fileRead import FileRead
+"""
+
+from .fileRead import FileRead
 import os
 import pandas as pd
 import numpy as np
+
 
 class Ons(FileRead):
 
     font = "ONS"
     extension = "xls"
-    def __init__(self, path = os.getcwd()):
-        super().__init__(path)
-        self.datas = self.read(self.name)
 
-    def listFiles(self):
+    def __init__(self, path=os.getcwd()):
+        super().__init__(path)
+        self.data = self.read(self.name)
+
+    def list_files(self):
         return super().list_files()
 
-    def read(self, name = None):
-        if name == None:
+    def read(self, name=None):
+        if name is None:
             return super().read()
         else:
             self.name = name
-            return self.__readLxs()
+            return self.__read_lxs()
 
-    def __readLxs(self):
-        fileOns = os.path.join(self.path, self.name+'.xls')
-        dataFlow = pd.read_excel(fileOns, shettname='Total', header=0, skiprows=5, index_col=0)
-        dataFlow.drop(np.NaN, inplace=True)
+    def __read_lxs(self):
+        file_ons = os.path.join(self.path, self.name+'.xls')
+        data_flow = pd.read_excel(file_ons, shettname='Total', header=0, skiprows=5, index_col=0)
+        data_flow.drop(np.NaN, inplace=True)
         
         aux = []
         dic = {'jan':'1', 'fev':'2', 'mar':'3', 'abr':'4', 'mai':'5', 'jun':'6', 'jul':'7', 'ago':'8', 'set':'9', 'out':'10', 'nov':'11', 'dez':'12'}
-        for i in dataFlow.index:
+        for i in data_flow.index:
             aux.append(i.replace(i[-8:-5], dic[i[-8:-5]]))
 
-        dataFlow.index = pd.to_datetime(aux, dayfirst=True)
-        codeColumn = [i.split(' (')[0] for i in dataFlow.axes[1]]
-        dataFlow.columns = codeColumn
+        data_flow.index = pd.to_datetime(aux, dayfirst=True)
+        code_column = [i.split(' (')[0] for i in data_flow.axes[1]]
+        data_flow.columns = code_column
 
-        return dataFlow.astype(float)
+        return data_flow.astype(float)
