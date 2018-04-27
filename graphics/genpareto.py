714077c47ed7fc12b763e52836b5852dde84ae2a
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-04-27 00:17:55 -0300

    Rename

---
+++ graphics/genpareto.py
@@ -0,0 +1,15 @@
+import scipy.stats as stat
+
+import plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as FF
+import colorlover as cl
+import cufflinks as cf
+
+from graphics.distribution_biuld import DistributionBiuld
+
+
+class GenPareto(object):
+
+    def __init__(self, arg):
+        pass
