dddbc17ad594bb4e71bf7a45fc23b56e97a54d5c
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-05-30 01:11:29 -0300

    Save txt

--- files/fileWrite.py
+++ files/fileWrite.py
@@ -3,11 +3,42 @@ Created on 21 de mar de 2018
 
 @author: clebson
 """
+import pandas as pd
+import os
 
 from files.files import Files
 
 
 class FileWrite(Files):
 
-    def __init__(self, params):
-        pass
+    def __init__(self, df, name=None):
+        self.df = df
+        self.name = name
+
+    def excel(self):
+        writer = pd.ExcelWriter('%s.xlsx' % self.name)
+        self.df.to_excel(writer)
+        writer.save()
+
+    def csv(self):
+        self.df.to_csv('%s.csv' % self.name)
+
+    def json(self):
+        self.df.to_json('%s.json' % self.name)
+
+    def txt(self, name_dir):
+        bool = self.df.isnull()
+        for i in self.df:
+            data = pd.to_datetime('1/1/1977')
+            name_arq = 'log_%s-lat_%s' % i
+            name_arq = name_arq.replace('.','_')
+            arq = open(os.path.join(os.path.join(os.getcwd(),name_dir), name_arq+'.txt'), 'w')
+            for j in self.df[i].index:
+                if bool[i][j]:
+                    j=-1
+                else:
+                    j=self.df[i][j]
+                arq.write('{:>6}{:>6}{:>6}{:>12}\n'.format(data.day, data.month,
+                                                           data.year, j))
+                data+=pd.DateOffset(days=1)
+            arq.close()
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 from files.cemaden import Cemaden
+from files.fileWrite import FileWrite
 
 import timeit
 
 if __name__ == '__main__':
     ini = timeit.default_timer()
     path = "/home/clebson/Documentos/Projetos/201405_output_horário"
-    dados = Cemaden(path)
-    print(dados.read())
+    dados = Cemaden(path).read()
+    FileWrite(dados).txt('output')
+    #print(dados.read())
 
     fim = timeit.default_timer()
     print('Duração: ', fim-ini)
