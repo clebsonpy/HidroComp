405993f192f8f360a599378866471d3b88edabba
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-08-15 00:02:14 -0300

    Pequenas modificações

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 
 import pandas as pd
 import plotly as py
-from files.ons import Ons
-
 import timeit
 
+from files.ons import Ons
 from series.flow import Flow
 
 if __name__ == '__main__':
@@ -13,11 +12,12 @@ if __name__ == '__main__':
     file = "/home/clebson/Documentos/Projetos/HidroComp1_8/dadosXingo.csv"
     dados = pd.read_csv(file, index_col=0, names=["Date", "XINGO"],
                         parse_dates=True)
+    print(dados.std())
     flow = Flow(data=dados, source='ONS')
     maximum = flow.maximum(station='XINGO')
     parcial = flow.parcial(station="XINGO", type_criterion="median",
                            type_threshold="stationary", type_event="drought",
-                           value_threshold=0.25, duration=5)
+                           value_threshold=569.475, duration=5)
     print(parcial.event_peaks())
     data, fig = parcial.plot_hydrogram('Estiagem')
     py.offline.plot(fig, filename='gráficos/estiagem.html')
