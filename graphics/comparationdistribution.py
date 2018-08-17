d8d1c303e5acb0f42af8f4023e82111a2f4ac336
Clebson <clebson.farias@delmiro.ufal.br>
2018-08-17 16:34:11 -0300

    Gráfico Autocorrelacao

--- graphics/comparationdistribution.py
+++ graphics/comparationdistribution.py
@@ -3,7 +3,7 @@ import plotly.graph_objs as go
 
 class ComparationDistribution(object):
 
-    def __init__(self, datas, type_function, name):
+    def __init__(self, datas, name, type_function=None):
         self.datas = datas
         self.type_function = type_function
         self.name = name
@@ -14,10 +14,10 @@ class ComparationDistribution(object):
 
         layout = dict(title="Probabilidade Acumulada",
                       showlegend=True,
-                      width=945, height=827,
+                      width=1890, height=827,
                       xaxis=bandxaxis,
                       yaxis=bandyaxis,
-                      font=dict(family='Time New Roman', size=34, color='rgb(0,0,0)'))
+                      font=dict(family='Time New Roman', size=28, color='rgb(0,0,0)'))
 
         fig = dict(data=self.datas, layout=layout)
         return self.datas, fig
--- main.py
+++ main.py
@@ -15,14 +15,18 @@ if __name__ == '__main__':
     flow = Flow(data=dados, source='ONS')
     flow.date(date_start="01/01/1995", date_end="31/12/2012")
     value_threshold = flow.data.mean()['XINGO'] - flow.data.std()['XINGO']
+    print(flow.data.mean()['XINGO'], flow.data.std()['XINGO'])
 #    maximum = flow.maximum(station='XINGO')
-    parcial = flow.parcial(station="XINGO", type_criterion='duration',
-                           type_threshold="events_by_year", type_event="flood",
-                           value_threshold=1.65, duration=0)
+    parcial = flow.parcial(station="XINGO", type_criterion='median',
+                           type_threshold="stationary", type_event="drought",
+                           value_threshold=0.25, duration=0)
+    
     print(parcial.event_peaks())
-    print(parcial.test_autocorrelation())
-    data, fig = parcial.plot_hydrogram('Cheia')
-    py.offline.plot(fig, filename='gráficos/Cheia.html')
+    print("N eventos: ", len(parcial.peaks))
+    test, lag1, lag2 = parcial.test_autocorrelation()
+    print("Autocorrelação: %s; lag1: %s; lag2: %s" % (test, lag1, lag2))
+    #data, fig = parcial.plot_hydrogram('Cheia')
+    #py.offline.plot(fig, filename='gráficos/Cheia.html')
 
     fim = timeit.default_timer()
     print('Duração: ', fim-ini)
