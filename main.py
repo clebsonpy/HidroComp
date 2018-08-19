a44bf267cfed9802ede8d6ac8a04e32c347e40cf
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-08-19 01:03:26 -0300

    Teste

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     print(flow.date_start, flow.date_end)
 #    value_threshold = flow.data.mean()['XINGO'] - flow.data.std()['XINGO']
 #    maximum = flow.maximum(station='XINGO')
-#    parcial = flow.parcial(station="XINGO", type_criterion='duration',
-#                           type_threshold="events_by_year", type_event="flood",
-#                           value_threshold=1.65, duration=0)
-#    print(parcial.event_peaks())
+    parcial = test.parcial(station="XINGO", type_criterion='duration',
+                           type_threshold="events_by_year", type_event="flood",
+                           value_threshold=1.65, duration=0)
+    print(parcial.event_peaks())
 #    print(parcial.test_autocorrelation())
 #    data, fig = parcial.plot_hydrogram('Cheia')
 #    py.offline.plot(fig, filename='gráficos/Cheia.html')
