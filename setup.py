c744e4a42b0275d76a3662ea141c5343128ddb85
Clebson Farias <clebson2007.farias@gmail.com>
2020-04-25 12:04:09 -0300

    requirements.txt

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [r.strip() for r in require]
 
 setup(
     name='hydrocomp',
-    version='0.1.4',
+    version='0.1.5',
     include_package_data=True,
     pacotes=find_packages('src'),
     long_description=long_description,
